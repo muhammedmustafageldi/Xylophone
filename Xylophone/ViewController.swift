c66fd21d56ca16927b0ca1e2efa69278972c4353
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-01-21 13:18:10 +0300

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Muhammed Mustafa Geldi on 21.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XylophoneTests/XylophoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XylophoneTests.swift
+//  XylophoneTests
+//
+//  Created by Muhammed Mustafa Geldi on 21.01.2025.
+//
+
+import Testing
+@testable import Xylophone
+
+struct XylophoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
