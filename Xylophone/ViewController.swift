90c7d5f45aa77053f4ee5e3bf3c6142de0c02ad8
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-01-21 16:35:32 +0300

    Application is completed.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Muhammed Mustafa Geldi on 21.01.2025.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(sender.currentTitle!)
+        
+        // Change button opacity
+        sender.alpha = 0.5
+        
+        // Add 0.2 second delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(_ soundName : String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
 }
 
